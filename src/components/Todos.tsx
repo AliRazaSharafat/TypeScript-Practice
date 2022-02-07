9dc3829adcf9e5a9d2e26d20121a6f9c15b017fc
EvapDeveloper <aliraza@evap.my>
2022-02-08 00:36:55 +0500

    initial commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Todo from "../models/Todo";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.items.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
